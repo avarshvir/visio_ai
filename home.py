93b64b05b6cb0e08e6273233f2c88d4209dbb40f
avarshvir <avarshvir@gmail.com>
2025-06-17 13:45:13 +0530

    frontend main file

--- home.py
+++ home.py
@@ -1,3 +1,9 @@
+# Copyright 2025 Arshvir
+# Licensed under the Apache License, Version 2.0
+# See LICENSE file in the project root for full license information.
+
+#---------------------------------------------------------------------#
+
 import streamlit as st
 import webbrowser
 import pandas as pd
